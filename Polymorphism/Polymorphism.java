9de2806746b8a1c9559f4fd9efef9f7e436b261a
EashwarSS <eashwarlucky@gmail.com>
2025-10-17 21:13:34 +0530

    Core_Java

---
+++ Polymorphism/Polymorphism.java
@@ -0,0 +1,20 @@
+// Polymorphism:
+// - Many behaviour ( same object or reference has different behaviours)
+
+// 1. Compile time polymorhism -- Overloading
+// 	add(int, int)
+// 	add(int, int, int)
+
+// 2. Run time polymorphism -- Overriding
+// 	A
+// 		add(int,int)
+// 	B
+// 		add(int, int)
+
+// File: PolymorphismExample.java
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        System.out.println("Hello, Polymorphism!");
+    }
+}
